7c0de23079f952d0632c90dc03d9ee9d571c4c95
Sonik <xsd@yandex.ru>
2022-11-20 22:06:04 +0300

    update

--- main.cpp
+++ main.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 
-struct Mobilephone {
+struct MobilePhone {
     double weight;
     bool is_broken;
     double price;
 };
 
 int main() {
-    int kol_tel;
-    std::cin >> kol_tel;
-    auto *tel = new Mobilephone[kol_tel];
-    for (int i = 0; i < kol_tel; ++i) {
+    int count_tel;
+    std::cin >> count_tel;
+    auto *tel = new MobilePhone[count_tel];
+    for (int i = 0; i < count_tel; ++i) {
         std::cin >> tel[i].weight >> tel[i].is_broken >> tel[i].price;
     }
     std::cout << "<MobilePhones>" << std::endl;
-    for (int i = 0; i < kol_tel; ++i) {
+    for (int i = 0; i < count_tel; ++i) {
         std::cout << "\t" << "<MobilePhone " << "id=\"" << i << "\"" << " weight="
                   << "\"" << tel[i].weight << "\"" << " is_broken=" << "\"" << ((tel[i].is_broken) ? "true" : "false")
                   << "\"" << " price=" << "\"" << tel[i].price << "\"" << "/>" << std::endl;
